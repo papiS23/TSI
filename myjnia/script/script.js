af3da43a7f9c81b5c09e02e57944711ae39d4790
papiS23 <bartekpapis03@gmail.com>
2020-11-30 14:46:41 +0100

    sdasd

--- myjnia/script/script.js
+++ myjnia/script/script.js
@@ -1,32 +1,34 @@
 const usluga = document.querySelectorAll('.container main form .usluga label input');
 const pojazd = document.querySelectorAll('.container main form .pojazd select option');
 const karta = document.querySelectorAll('.container main form .karta label input');
+const wynik = document.querySelector('#cena');
+const submit = document.querySelector('#submit');
 
 let cena = 0;
 
 function oblicz_usluge(){
-for(let i = 0; i< usluga.length; i++){
-    if(usluga[i].checked == true){
-        cena += parseInt(usluga[i].value);
+    for(let i = 0; i< usluga.length; i++){
+        if(usluga[i].checked == true){
+            cena += parseInt(usluga[i].getAttribute("value"));
+            console.log(usluga[i].getAttribute("value"));
+        }
     }
-}
-for(let i = 0; i < pojazd.length; i++){
-    if(pojazd[i].checked == true){
-        cena = cena * parseInt(pojazd[i].value);
-}
-}
-for(let i = 0; i < karta.length; i++){
-    if(karta[i].checked == true){
-        cena = cena * parseInt(karta[i].value);
+    for(let i = 0; i < pojazd.length; i++){
+        if(pojazd[i].selected == true){
+            cena = cena * parseInt(pojazd[i].getAttribute("value"));
+            console.log(pojazd[i].getAttribute("value"));
     }
-}
-return cena;
+    }
+    for(let i = 0; i < karta.length; i++){
+        if(karta[i].checked == true){
+            cena = cena * parseInt(karta[i].getAttribute("value"));
+            console.log(karta[i].getAttribute("value"));
+        }
+    }
+    wynik.innerHTML = `Cena wynosi: ${cena}zł`;
 }
 
-const submit = document.querySelector('#submit');
 submit.onclick = oblicz_usluge();
 
-const wynik = document.querySelector('#cena');
-wynik.innerHTML = `Cena wynosi: ${oblicz_usluge()}zł`;
 
-console.log(oblicz_usluge());
+
