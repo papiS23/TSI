39dc40ba9c1609a9bcd4726ad3da284a17ea31cc
papiS23 <bartekpapis03@gmail.com>
2020-12-01 22:15:17 +0100

    proby proby

---
+++ autonomy/scripts/script.js
@@ -0,0 +1,8 @@
+const all_a = document.querySelectorAll('.activate');
+console.log(all_a);
+
+
+
+
+
+    
--- myjnia/script/script.js
+++ myjnia/script/script.js
@@ -7,27 +7,26 @@ const submit = document.querySelector('#submit');
 let cena = 0;
 
 
-submit.addEventListener("click", function(evt){
-    evt.preventDefault();
-    cena = 0; //zerowanie ceny
-    for(let i = 0; i< usluga.length; i++){
-        if(usluga[i].checked == true){
-            cena += parseInt(usluga[i].getAttribute("value"));
+submit.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        cena = 0; //zerowanie ceny
+        for (let i = 0; i < usluga.length; i++) {
+            if (usluga[i].checked == true) {
+                cena += parseInt(usluga[i].getAttribute("value"));
+            }
         }
-    }
-    for(let i = 0; i < pojazd.length; i++){
-        if(pojazd[i].selected == true){
-            cena = cena * parseFloat(pojazd[i].getAttribute("value"));
-    }
-    }
-    for(let i = 0; i < karta.length; i++){
-        if(karta[i].checked == true){
-            cena = cena * parseFloat(karta[i].getAttribute("value"));
-            console.log(parseFloat(karta[i].getAttribute("value")))
+        for (let i = 0; i < pojazd.length; i++) {
+            if (pojazd[i].selected == true) {
+                cena = cena * parseFloat(pojazd[i].getAttribute("value"));
+            }
         }
-    }
-    wynik.innerHTML = `Cena wynosi: ${cena}zł`;
-})
+        for (let i = 0; i < karta.length; i++) {
+            if (karta[i].checked == true) {
+                cena = cena * parseFloat(karta[i].getAttribute("value"));
+            }
+        }
+        wynik.innerHTML = `Cena wynosi: ${cena}zł`;
+    })
 
 
 
