7b38290e82c00e46a4eef6523a58fa26e219cd45
papiS23 <bartekpapis03@gmail.com>
2020-12-02 22:54:07 +0100

    dalsze proby

--- autonomy/scripts/script.js
+++ autonomy/scripts/script.js
@@ -1,8 +1,14 @@
 const all_a = document.querySelectorAll('.activate');
 console.log(all_a);
 
-
-
-
-
-    
+all_a.addEventListener("mousedown", () => {
+        for (let i = 0; i < all_a.length; i++) {
+            all_a[i].classList.remove('active');
+    }
+    for(let i = 0; i < all_a.length; i++){
+        all_a[i].addEventListener("mouseup", ()=>{
+            all_a[i].classList.add('active');
+        })
+    }
+        
+})
