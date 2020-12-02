785e618584e1365e52317ab260ed8fcafc3aee75
papiS23 <bartekpapis03@gmail.com>
2020-12-02 23:09:28 +0100

    UDAŁO SIĘ!

--- autonomy/scripts/script.js
+++ autonomy/scripts/script.js
@@ -1,14 +1,14 @@
-const all_a = document.querySelectorAll('.activate');
+const all_a = document.querySelector('.all_a');
+const a = document.querySelectorAll('.activate');
 console.log(all_a);
 
 all_a.addEventListener("mousedown", () => {
-        for (let i = 0; i < all_a.length; i++) {
-            all_a[i].classList.remove('active');
-    }
-    for(let i = 0; i < all_a.length; i++){
-        all_a[i].addEventListener("mouseup", ()=>{
-            all_a[i].classList.add('active');
+        for (let i = 0; i < a.length; i++) {
+            a[i].classList.remove('active');
+        }
+        for(let i = 0; i < a.length; i++){
+        a[i].addEventListener("mouseup", ()=>{
+            a[i].classList.add('active');
         })
-    }
-        
+        }
 })
