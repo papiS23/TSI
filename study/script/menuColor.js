053c1d47330238cdcd518c4366500812a04d2714
Bartłomiej Papis <72127774+papiS23@users.noreply.github.com>
2021-01-11 23:57:27 +0100

    w koncu

--- study/script/menuColor.js
+++ study/script/menuColor.js
@@ -1,6 +1,7 @@
 const li = document.querySelectorAll('#menu ul li');
 const logo = document.querySelector('#logo');
-const offert_button = document.querySelector('#offert_button');
+const main_button = document.querySelector('#offert_button');
+const offert_button = document.querySelector('#offert_sg');
 
 for(let i=0; i<li.length;i++){
     li[i].addEventListener('click', ()=>{
@@ -18,9 +19,16 @@ logo.addEventListener('click', ()=>{
     li[0].classList.add('active');
 })
 
-offert_button.addEventListener('click', ()=>{
+main_button.addEventListener('click', ()=>{
     for(let i=0; i<li.length; i++){
         li[i].classList.remove('active');
     }
     li[1].classList.add('active');
+})
+
+offert_button.addEventListener('click', ()=>{
+    for(let i=0; i<li.length; i++){
+        li[i].classList.remove('active');
+    }
+    li[0].classList.add('active');
 })
