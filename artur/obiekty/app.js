9495d8dd8ad3e18874551d75786ad280b36f2422
Bartłomiej Papis <72127774+papiS23@users.noreply.github.com>
2021-06-04 20:31:13 +0200

    wstep obiekty

---
+++ artur/obiekty/app.js
@@ -0,0 +1,9 @@
+const input = document.querySelector(".main__input");
+const submit = document.querySelector(".main__submit");
+const list = document.querySelector(".main__list");
+
+class item
+
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+});
