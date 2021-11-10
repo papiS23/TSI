f80ac9449008d75b7516c9fcc325ca7dc67b8a3a
Bartłomiej Papis <72127774+papiS23@users.noreply.github.com>
2021-11-10 09:27:39 +0100

    dokonczyc

---
+++ ZadanieEgzaminacyjne4/futbol.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rozgrywki futbolowe</title>
+    <link rel="stylesheet" href="./styl.css">
+</head>
+<body>
+    <header class="baner">
+        <h2>Światowe rozgrywki piłkarskie</h2>
+        <img src="./obraz1.jpg" alt="boisko">
+    </header>
+    <section class="mecze"></section>
+    <main class="glowny"></main>
+    <aside class="lewy"></aside>
+    <aside class="prawy"></aside>
+</body>
+</html>
